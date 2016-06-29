6c7affa555bb2b2b5ac1c0bc4c4a36455ea4f61c
AMAN TRIPATHI <aman@circarconsulting.com>
2016-06-29 12:01:51 +0530

    some changes for friendship tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625073059) do
+ActiveRecord::Schema.define(version: 20160628111624) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.string   "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
